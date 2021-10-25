c233d3ecec41dcdd4d423fed9bd1dc886348d5b3
SuporteHospidata <92329205+SuporteHospidata@users.noreply.github.com>
2021-10-25 15:07:37 -0300

    V.0.0.3
    Alterado as datas

--- val_user_HD.sql
+++ val_user_HD.sql
@@ -14,7 +14,7 @@ abrir_menu_flutuante = 'f',
 nao_receber_permissoes_automaticas = 't'
 WHERE id_usuario = (select id_usuario from conf.usuarios where username = 'HD_SUPORTE');  -- User
 
------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------
 
 UPDATE conf.usuarios set expiration_date = '2030-10-10', --expira  date
  max_days_innative = 9999, max_bad_logins = 9999, time_out = 9999,  
@@ -25,9 +25,9 @@ UPDATE conf.usuarios set expiration_date = '2030-10-10', --expira  date
  permite_voltar_esc_lookup = 't', 
  abrir_menu_flutuante = 'f', 
  nao_receber_permissoes_automaticas = 't'
-WHERE id_usuario = (select id_usuario from conf.usuarios where username = 'HD_SUPORTE'); --User date 
+WHERE id_usuario = (select id_usuario from conf.usuarios where username = 'HD_SUPORTE'); --User 
 
------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
 
 UPDATE conf.usuarios set expiration_date = '2030-10-10', --expira date 
   max_days_innative = 9999, 
@@ -37,3 +37,5 @@ UPDATE conf.usuarios set expiration_date = '2030-10-10', --expira date
 recebe_newsletter = 'f', gravacao_automatica = 'f', 
  permite_voltar_esc_lookup = 't', abrir_menu_flutuante = 'f', 
  nao_receber_permissoes_automaticas = 't' WHERE id_usuario = (select id_usuario from conf.usuarios where username = 'HD_FATURAMENTO'); --User
+ 
+ -----------------------------------------------------------------------------------------------------------------------------------------
