02e3c54570893353070254381296cbaa8c350bbc
SuporteHospidata <92329205+SuporteHospidata@users.noreply.github.com>
2022-01-11 10:43:45 -0300

    Adicionado a alteração da versão no banco
    Adicionado a alteração da versão no banco

--- versão_banco.sql
+++ versão_banco.sql
@@ -3,3 +3,9 @@
 select versao, releases from conf.parametros_conf;
 
 --normalmente usando quando vai abrir aplicação e aparece a mensagem "Sistemas incompativel com a versão do banco de dados"
+
+--Alterar a versão no banco
+
+update conf.parametros_conf
+set releases = XX --setar a versão, ultimo 2 numeros
+where id_parametros_conf;
