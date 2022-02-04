ccd1c65466684d30facad7e7fa81c7bef8ca7316
SuporteHospidata <92329205+SuporteHospidata@users.noreply.github.com>
2022-02-04 16:10:15 -0300

    adicionar o arquivo .Bat para backup e compactar
    adicionar o arquivo .Bat para backup e compactar , para ele funcionar precisa execitaiveis 7zFm e 7z tanto na sistemasHD quanto na pasta onde será armazenados os backup

---
+++ backup_compactado.bat
@@ -0,0 +1,21 @@
+set PGPASSWORD=s@tt30hd2013
+set HOST=192.168.0.100
+set PORT=5432
+set CLIENT=bd0282
+set DIR_OUT=Y:\backups
+rem ------------------------------------------------------------------------------------------------------------------------------------------
+
+rem ------Backup com Util---------------------
+rem C:\SistemasHD\util.exe -B C:\bkp
+
+rem -----------backup direto pg_dump --------
+rem -- 
+
+pg_dumpall.exe -h %HOST% -p %PORT% -U hd_suporte -v -g > %DIR_OUT%\Usuarios.sql
+pause
+pg_dump.exe -h %HOST% -p %PORT% -U hd_suporte -v -C %CLIENT% > %DIR_OUT%\Backup%CLIENT%%date:~6,4%%date:~3,2%%date:~0,2%.sql
+pause
+7z.exe a %DIR_OUT%\Backup%CLIENT%%date:~6,4%%date:~3,2%%date:~0,2%.zip %DIR_OUT%\*.sql
+pause
+del "%DIR_OUT%\*.sql"
+pause
