591cc7ea0fa87fb96eb72ada954853a492fbbe87
SuporteHospidata <92329205+SuporteHospidata@users.noreply.github.com>
2022-02-04 16:19:32 -0300

    Update backup_compactado.bat

--- backup_compactado.bat
+++ backup_compactado.bat
@@ -2,10 +2,10 @@
 que estão localizados nesta pasta : \\HDSERVER13\público\willian\backup bat
 
 set PGPASSWORD=s@tt30hd2013
-set HOST=192.168.0.100
-set PORT=5432
-set CLIENT=bd0282
-set DIR_OUT=Y:\backups
+set HOST=XXX.XXX.X.XXX --IP
+set PORT=XXXX --PORTA
+set CLIENT=bdXXXX --BASE
+set DIR_OUT=XXXXXX --DIR onde backp sera armazenados
 rem ------------------------------------------------------------------------------------------------------------------------------------------
 
 rem ------Backup com Util---------------------
