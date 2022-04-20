38d4ff2d19ed5416499804eb90caab4a4cb6b4ea
SuporteHospidata <92329205+SuporteHospidata@users.noreply.github.com>
2022-04-20 18:59:46 -0300

    Update backup_compactado.bat

--- backup_compactado.bat
+++ backup_compactado.bat
@@ -9,7 +9,7 @@ rem ----------------------------------------------------------------------------
 cd \Program Files\PostgreSQL\9.2\bin -- pra utilizar o pg_dump diretamente da pasta do postgres (caso estação possua o postgres instalado)
 pg_dumpall.exe -h %HOST% -p %PORT% -U hd_faturamento -v -g > %DIR_OUT%\Usuarios.sql
 
-rem pg_dump.exe -h %HOST% -p %PORT% -U hd_faturamento -v -C %CLIENT% > %DIR_OUT%\Backup%CLIENT%%date:~6,4%%date:~3,2%%date:~0,2%.sql
+pg_dump.exe -h %HOST% -p %PORT% -U hd_faturamento -v -C %CLIENT% > %DIR_OUT%\Backup%CLIENT%%date:~6,4%%date:~3,2%%date:~0,2%.sql
 
 cd xxxxxx -- local onde está o 7zip para compactação 
 
