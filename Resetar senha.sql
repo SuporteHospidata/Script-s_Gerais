4a28f8f846dd46c2d5d2c05f6ccc49025379ee73
SuporteHospidata <92329205+SuporteHospidata@users.noreply.github.com>
2022-04-24 18:21:28 -0300

    Update Resetar senha.sql

--- Resetar senha.sql
+++ Resetar senha.sql
@@ -1,1 +1,3 @@
-update conf.usuarios set muda_password = 'T'; -- altera para solicitar nova senha no proximo login
+-- altera para solicitar nova senha no proximo login, exceto usuarios que sejam hd_% (faturamento, suporte, etc...)
+
+update conf.usuarios set muda_password = 'T' where not username ilike ('HD_%'); 
