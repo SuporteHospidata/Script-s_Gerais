e336c7b799c4aef7492cd07ce049b90c35657812
SuporteHospidata <92329205+SuporteHospidata@users.noreply.github.com>
2022-05-13 09:30:25 -0300

    Create alterar qtd do estoque.sql

---
+++ alterar qtd do estoque.sql
@@ -0,0 +1,12 @@
+-- visualizar produto e estoque no banco
+ 
+select * from sigh.produtos where nm_produto ilike '%DEXAMETASONA 4mg/ml 2,5ml%' -- seleciona id do produto
+select * from sigh.estoques -- seleciona o estoque
+select * from sigh.estoques_produtos where cod_produto = 1301 and cod_estoque = 81 -- filtra produto por estoque
+
+
+-- update para mudar estoque 
+begin;
+update sigh.estoques_produtos set qtd_unid_consumo = 36.500000000000000 where cod_produto = 1301 and cod_estoque = 81 -- atualiza o estoque do produto
+end;
+commit;
